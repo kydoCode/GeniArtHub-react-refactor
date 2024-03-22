27248ff99644f0af7c913b0daf509a3edefca7c3
JackAdamsJenkins <clensx@gmail.com>
2024-03-22 15:34:45 +0100

    Add: delete

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,37 +1,40 @@
-// Récupérer le panier depuis le localStorage
-const panier = JSON.parse(localStorage.getItem("cart")) || [];
+const cart = JSON.parse(localStorage.getItem("cart")) || []
+let datas = []
+async function init() {
+    datas = await getDatas()
+    populateData()
+    updateTotal()
+    listenDelete()
+}
+init()
 
-// Si le panier est vide, afficher le message : Votre panier est vide. Veuillez ajouter au moins un article à votre panier.
-if(panier.length === 0){
-    document.querySelector("#panier").innerText = "Votre panier est vide. Veuillez ajouter au moins un article à votre panier.";
+async function getDatas() {
+    const req = await fetch("http://localhost:3000/api/products")
+    return await req.json()
 }
 
-// Création d'une fonction asynchrone qui va parcourir tous les éléments du panier et faire la correlation avec les id et les formats pour afficher les prix et les images et les titres
-async function displayCart(){
-    // Parcourir chacun des produits dans le panier
-    panier.forEach(async el => {
-        // Faire appel à l'api pour récupérer les informations du produit
-        const req = await fetch(`http://localhost:3000/api/products/${el.id}`)
-        const datas = await req.json()
+function populateData(){
+    // Parcourir le localStorage
+    cart.forEach(el => {
+        // Récupérer l'oeuvre correspondante dans datas
+        const product = datas.find(product => product._id === el.id && product.declinaisons[el.taille] === el.format)
 
-        // On parcours les données en récupérant : l'id, le format, la quantité, le prix et l'image
         const article = `<article class="article">
-                            <img src="${datas.image}" alt="${datas.titre}">
-                            <h3>${datas.titre}</h3>
+                            <img src="${product.image}" alt="${product.titre}">
+                            <h3>${product.titre}</h3>
                             <p>Format : ${el.taille}</p>
-                            <p class="price">${datas.declinaisons[el.index].prix}€</p>
+                            <p class="price">${product.declinaisons[el.index].prix}€</p>
                             <div>
                                 <p>Quantité : </p>
-                                <input type="number" value="${el.quantite}" data-id="${el.id}" data-format="${el.taille} min="1">
+                                <input type="number" value="${el.quantite}" data-id="${el.id}" data-format="${el.taille}" min="1">
                             </div>
                             <a href="#">Supprimer</a>
                         </article>`
         // Injecter les articles
         document.querySelector('#panier').insertAdjacentHTML('beforeend', article)
-        updateTotal()
+
     })
 }
-displayCart()
 
 // Une petite fonction qui va mettre à jour le prix total et le nombre total d'articles
 function updateTotal(){
@@ -55,3 +58,33 @@ function updateTotal(){
     montantTotal.innerText = totalAmount.toFixed(2)
     numberItem()
 }
+
+// Fonction qui écoute le clique sur le bouton supprimer
+function listenDelete(){
+    const deleteButtons = document.querySelectorAll('.article a')
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+            const article = e.target.closest('.article')
+            const id = article.querySelector('input').dataset.id
+            const format = article.querySelector('input').dataset.format
+
+            // On cherche l'index dans le panier
+            const index = cart.findIndex(el => el.id === id && el.taille === format)
+
+            // On supprime l'élément du panier
+            cart.splice(index, 1)
+            // On met à jour le localStorage
+            localStorage.setItem("cart", JSON.stringify(cart))
+
+            // On supprime l'article du dom
+            article.remove()
+
+            // On met à jour le total
+            updateTotal()
+
+        })
+    })
+}
+
+// Fonction qui écoute le changement de quantité
